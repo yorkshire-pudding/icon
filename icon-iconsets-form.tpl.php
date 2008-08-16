cd60e2d17bf5e6ae15bb90641ebc5edbe9a1be62
Joakim Stai <joakimstai@gmail.com>
2008-08-16 23:13:29 +0000

    Restricted icon sets to their type when dragging them in the admin form table.

--- icon-iconsets-form.tpl.php
+++ icon-iconsets-form.tpl.php
@@ -10,7 +10,7 @@
  * - $types: Names of icon set types.
  * - $buttons: Submit and Reset buttons as well as hidden form elements.
  *
- * Each $iconsets[$type] contains.
+ * Each $iconsets[$type] contains:
  * - $screenshot: Its screenshot (or 'no screenshot' text).
  * - $name: The name of the icon set.
  * - $description: Its description.
@@ -24,6 +24,7 @@
 <?php
   // Add table javascript.
   drupal_add_js('misc/tableheader.js');
+  drupal_add_js(drupal_get_path('module', 'icon') .'/icon.js');
   foreach ($iconsets as $type => $sets) {
     // TODO: Restrict dragging of an item to within its section.
     drupal_add_tabledrag('iconsets', 'order', 'sibling', 'iconset-weight', 'iconset-weight-'. $type);
@@ -41,14 +42,14 @@
   <tbody>
   <?php $row = 0; ?>
   <?php foreach ($iconsets as $type => $sets): ?>
-    <tr class="region type-<?php print $type; ?>">
+    <tr class="iconset-type type-<?php print $type; ?>">
       <td colspan="4" class="region"><?php print $types[$type]; ?></td>
     </tr>
     <tr class="type-message type-<?php print $type; ?>-message <?php print empty($iconsets[$type]) ? 'type-empty' : 'type-populated'; ?>">
       <td colspan="4"><em><?php print t('No icon sets'); ?></em></td>
     </tr>
     <?php foreach ($iconsets[$type] as $iconset): ?>
-    <tr class="draggable <?php print $row % 2 == 0 ? 'odd' : 'even'; ?>">
+    <tr class="draggable <?php print $row % 2 == 0 ? 'odd' : 'even'; ?> type-<?php print $type; ?>">
       <td><?php print $iconset->screenshot; ?></td>
       <td class="iconset-info"><h2><?php print $iconset->name; ?></h2><div class="description"><?php print $iconset->description; ?></div></td>
       <td><?php print $iconset->status_checkbox; ?></td>
@@ -59,5 +60,4 @@
   <?php endforeach; ?>
   </tbody>
 </table>
-
 <?php print $buttons; ?>
