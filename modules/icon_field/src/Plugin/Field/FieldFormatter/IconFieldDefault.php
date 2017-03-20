38eec3a77147d971122be4e07d35515c41a776ca
sjpeters79 <sjpeters79@2897533.no-reply.drupal.org>
2017-03-20 13:26:09 -0500

    Issue #2529032 by sylus, inders, sjpeters79, markcarver: Upgrade To Drupal 8

--- modules/icon_field/src/Plugin/Field/FieldFormatter/IconFieldDefault.php
+++ modules/icon_field/src/Plugin/Field/FieldFormatter/IconFieldDefault.php
@@ -1,29 +1,53 @@
-<?php /**
+<?php
+
+namespace Drupal\icon_field\Plugin\Field\FieldFormatter;
+
+/**
  * @file
  * Contains \Drupal\icon_field\Plugin\Field\FieldFormatter\IconFieldDefault.
  */
 
-namespace Drupal\icon_field\Plugin\Field\FieldFormatter;
-
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal;
 
 /**
+ * Plugin implementation of the 'icon_field_default' formatter.
+ *
  * @FieldFormatter(
  *  id = "icon_field_default",
  *  label = @Translation("Icon"),
- *  field_types = {"icon_field"}
+ *  field_types = {
+ *    "icon"
+ *  }
  * )
  */
 class IconFieldDefault extends FormatterBase {
 
   /**
-   * @FIXME
-   * Move all logic relating to the icon_field_default formatter into this
-   * class. For more information, see:
-   *
-   * https://www.drupal.org/node/1805846
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterInterface.php/interface/FormatterInterface/8
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterBase.php/class/FormatterBase/8
+   * {@inheritdoc}
    */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = array();
+    $entity = $items->getEntity();
+    $uri = $entity->uri;
+    foreach ($items as $delta => $item) {
+      if ($this->getSetting('link_to_content')) {
+        $options = array('html' => TRUE, 'attributes' => array());
+        if (isset($uri['options']) and !empty($uri['options'])) {
+          $options = array_merge($uri['options'], $options);
+        }
+      }
+      else {
+        $elements[$delta] = array(
+          '#type' => 'icon',
+          '#icon' => $item['icon'],
+          '#tag' => 'p',
+        );
+      }
+    }
+
+    return $elements;
+  }
 
 }
--- modules/icon_field/src/Plugin/Field/FieldWidget/IconField.php
+++ modules/icon_field/src/Plugin/Field/FieldWidget/IconField.php
@@ -1,29 +1,53 @@
-<?php /**
- * @file
- * Contains \Drupal\icon_field\Plugin\Field\FieldWidget\IconField.
- */
+<?php
 
 namespace Drupal\icon_field\Plugin\Field\FieldWidget;
 
+/**
+ * @file
+ * Contains \Drupal\icon_field\Plugin\Field\FieldWidget\IconField.
+ */
+use Drupal;
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
+ * Plugin implementation of the 'icon_default_widget' widget.
+ *
  * @FieldWidget(
- *  id = "icon_field",
- *  label = @Translation("Icon"),
- *  field_types = {"icon_field"}
+ *   id = "icon_default_widget",
+ *   label = @Translation("Icon"),
+ *   field_types = {
+ *     "icon"
+ *   }
  * )
  */
 class IconField extends WidgetBase {
 
   /**
-   * @FIXME
-   * Move all logic relating to the icon_field widget into this class.
-   * For more information, see:
-   *
-   * https://www.drupal.org/node/1796000
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21WidgetInterface.php/interface/WidgetInterface/8
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21WidgetBase.php/class/WidgetBase/8
+   * {@inheritdoc}
    */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $value = isset($items[$delta]->value) ? $items[$delta]->value : '';
+    $element += array(
+      '#type' => 'textfield',
+      '#default_value' => $value,
+      '#element_validate' => array(
+        array($this, 'validate'),
+      ),
+    );
+    return array('icon' => $element);
+  }
+
+  /**
+   * Validate the icon field.
+   */
+  public function validate($element, FormStateInterface $form_state) {
+    $value = $element['#icon'];
+    if (strlen($value) == 0) {
+      $form_state->setValueForElement($element, '');
+      return;
+    }
+  }
 
 }
